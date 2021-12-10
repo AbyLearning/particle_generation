ef6013c25bb23b37b04c0ba4325df21fe3b36291
abyuthup <abyuthup@gmail.com>
2021-12-10 11:47:37 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/particle_generation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.particle_generation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: ParticleWidget(),
+    );
+  }
+}
+
+class ParticleWidget extends StatefulWidget {
+  const ParticleWidget();
+
+  @override
+  _ParticleWidgetState createState() => _ParticleWidgetState();
+}
+
+class _ParticleWidgetState extends State<ParticleWidget>
+    with SingleTickerProviderStateMixin {
+  late List<Particle> particles;
+  // Random rd = Random(DateTime.now().millisecondsSinceEpoch);
+  Random rd = Random();
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addListener(() {
+        setState(() {
+          // The state that has changed here is the animation object’s value.
+        });
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.repeat();
+        } else if (status == AnimationStatus.dismissed) {
+         // controller.forward();
+        }
+      });
+    controller.forward();
+
+    particles = List<Particle>.generate(200, (index) {
+      var particle = Particle();
+      particle.position = Offset(-1, -1);
+      particle.radius = rd.nextDouble() * 15;
+      particle.speed = rd.nextDouble() * 2.0;
+      particle.angle = rd.nextDouble() * 2 * pi;
+      particle.color=getRandomColor(rd);
+      return particle;
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CustomPaint(
+        painter: ParticlePainter(particles, rd),
+      ),
+    );
+  }
+}
+
+Offset getVelocity(double angle, double speed) {
+  var dx = speed * cos(angle);
+  var dy = speed * sin(angle);
+  return Offset(dx, dy);
+}
+Color getRandomColor(Random random)
+{
+  int r=random.nextInt(255);
+  int g=random.nextInt(255);
+  int b=random.nextInt(255);
+  return Color.fromARGB(255, r, g, b);
+}
+class ParticlePainter extends CustomPainter {
+  List<Particle> particles;
+  Random random;
+
+  ParticlePainter(this.particles, this.random);
+
+  Paint circlePaint = Paint();
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    particles.forEach((particle) {
+      var velocity = getVelocity(particle.angle, particle.speed);
+      var dx = particle.position.dx + velocity.dx;
+      var dy = particle.position.dy + velocity.dy;
+
+      if (dx < 0 || dx > size.width) {
+        dx = random.nextDouble() * size.width;
+      }
+      if (dy < 0 || dy > size.height) {
+        dy = random.nextDouble() * size.height;
+      }
+
+      particle.position = Offset(dx, dy);
+      circlePaint.color=particle.color;
+      canvas.drawCircle(particle.position, particle.radius, circlePaint);
+    });
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class Particle {
+  Offset position = Offset(-1, -1);
+  double speed = 0.0;
+  double angle = 0.0;
+  double radius = 0.0;
+  double alpha = 0.0;
+  Color color = Colors.black;
+}
